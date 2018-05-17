e5ba106e4dafee7bd0a1c8d88465f1409aa87e8e
211211 <nhquan307@outlook.com>
2018-05-18 01:01:15 +0700

    TEMP commit

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const serviceAccount = require('./service_account.json');
+const service_account_1 = require("./service_account");
 const createUser = require('./create_user');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(service_account_1.default),
     databaseURL: "https://one-time-password-b0338.firebaseio.com"
 });
 exports.createUser = functions.https.onRequest(createUser);
---
+++ functions/lib/service_account.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    "type": "service_account",
+    "project_id": "one-time-password-b0338",
+    "private_key_id": "cec898449c3faffdf7e30dc4fabb48c49b450e90",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChGL4XL39P4lcQ\nM5UEs96sZr0Dt4WGJRXeaDxwP7mV8zstGu0IvNXB18noTybenxEmFkLAycCkb35D\ncmqMDlJ9RGvOy9ZQ3MHnccrv+GHeHSlniYYs0vsaBTQZslB4MHdEPpUkkFJC/MhY\n7WHs8GxzFvbFT6iRhstCya/TkzAkTif+zY/s4xGOC+Z447G1Vbv7XYIJRBGD5dab\nb9/tMmqwPP84TSwcDKdhHj7JfM4Q6K9apPnJBCJ4Lakv4sujx4l7YOX7tbHzbgOS\nGqnhHwAfG2qgYvvFWX2iw8UWJrWujgUbFj/JM5TY6h3jZ/S1+doKOWK5DwegKcu9\nNl9Qi/dlAgMBAAECggEAIQKF6RBVgRP7DRnUvjrxNNh0gK95T8yHl6Q6RjH4oE5P\n4OU0jubK0kaaW9zkNRfZFrnXAdQVglxKPh3S0VIZ+wsrIBvKlZU2smsxz0xRGYdW\nNZ4LfWHaDDHmxCEytf4QYk6ElxJvmMRddUNnHnsBbe/Vrwasnu1E9zGGss7lPXrm\n/CVwwv5wNid0Q7QaQ9p15jiiXx40DyCG3miAV5HsH44jxA1FpXiE9pvRd0mLfgI0\n3VNqeA4JCfJ25ZxAwTLo3u5TA/dFq/VrlUXv/p7Y2XArQ+bFWTEOY/ZbIqN4JZ7+\no1lCq0ki0p6bkQL1HNU2Qgv0m9nmDYtZc57cuZ+g/QKBgQDXyB1KaBzkYu4jhx0K\nm1TbRzxEOXd2uYN/ySg3yVsdcnTr7wccT1ca4z5mMd2J22A8WkfkZBtresSEOuDG\nN24y7vYb6zflXpwYEK0i/bjtJPhEMmhPZyKthx9bsawhFNwAL1UX7dPZ1RxgOL9/\nkjX3FpvCyTWps78PWtJoICwJ6wKBgQC/H126xKuUTud/5WWfHWvFGyzqeNdG1HA3\nnbwPxC0+nFljXf6jOG6p2LvfUc/6KDHKTQLwAJHXh/jAJjntOTCPfk6AIbgVEwks\nhFqI6tr/vKOVrmSkGGWaHt5mcBOph7JvBS/RPgejJwFXazSlaWp/UoIkZFDRoPnh\ntfBIiqHf7wKBgHiq3r5iVG3faP6TK0loLd7mCiy42jaZbZEDQ6tWvc5M2Fer9pT9\nRV+y3o01q4bRGBMIBZYR6UeEgurTJimSi9yNxzE7q6ln6/CFOt0Ld+OdvfwdGDn3\nGexiWMXlKLXjPVgZIAPeqo7xXwnsEiLb97Oyf+5AqPK6osiKaIl0HEx7AoGBALXU\nE4oyvcI1Z01oY0wD45n8hrj0CmuahMCE0nx1eU4N1YUR0cZbU/1G80Y9QY6PWv9K\nUTcgc6AH/oJgW8/zDt4Vnn2MluOF3r55lOCZ/4w6Xa9z4M+1XkRIhfITQsGRFhNF\nbf618vInk8Elf0q3hdhoGOSig7lMWy9/toBtVCAFAoGAQg7wHQTSJlfFtHzmwRCs\nwyH13IJyDP46YHFzcOm81gjM5W0wf2ENJkUZQRM/hcq1jOw7xvb+1A/7yHcoDhIa\n6hq9dPM/Zu2UsZ8hsMvpvQgh6ABf6eVHcSLz0hi2g3GCsTbcJ5hQjB0KPPI8Dux+\nCHf9SuLppIyPfxT8Q8G6/uA=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-ui0y3@one-time-password-b0338.iam.gserviceaccount.com",
+    "client_id": "112350548489325581425",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ui0y3%40one-time-password-b0338.iam.gserviceaccount.com"
+};
+//# sourceMappingURL=service_account.js.map
